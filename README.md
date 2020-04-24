cdb8c177cb7c15c0a27f8daab14b9b6b4ebdb5f8
Luke Pereira <lukejoepereira@gmail.com>
2020-04-24 03:26:38 -0400

    Refactor

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # LaTex CI Compiler
 
-This repo compiles modified .tex files and commits the rendered .pdfs on push to remote using a combination of modified GitHub actions.
+    This repo compiles any modified .tex files and will commit the rendered .pdfs on push to remote using GitHub actions.
+
+## Contents
+
+---
+
+### Notes
+- [Stochastic Modeling and Queuing Theory (2020)](documents/2020-performance-analysis)
+
+### Essays
+- [Dynamical Systems (2020)](documents/dynamical-systems)
+- [Chaos (2020)](documents/2020-chaos-essay)
+- [RSA Encryption (2017)](documents/2017-rsa-essay)
+- [Machine Learning (2017)](documents/2017-machine-learning-essay)
+
+### Assignments
+- [Recursive Generating Functions (2020)](documents/2020-combinatorics-generating-functions)
+- [Markov Chains & Chaotic Symbolic Dynamics (2020)](documents/2020-chaotic-systems)
+
+### Templates
+- [Homework](documents/homework-template) 
---
+++ compiler/entrypoint.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+set -e
+
+warn() {
+  echo "::warning :: $1"
+}
+
+error() {
+  echo "::error :: $1"
+  exit 1
+}
+
+root_file="$1"
+working_directory="$2"
+compiler="$3"
+args="$4"
+extra_packages="$5"
+extra_system_packages="$6"
+changed_files="$7"
+
+if [ -z "$root_file" ] && [ -z "$changed_files" ]; then
+  error "Input 'root_file' or 'changed_files' is missing."
+fi
+
+if [ -z "$compiler" ] && [ -z "$args" ]; then
+  warn "Input 'compiler' and 'args' are both empty. Reset them to default values."
+  compiler="latexmk"
+  args="-pdf -file-line-error -interaction=nonstopmode"
+fi
+
+if [ -n "$extra_system_packages" ]; then
+  for pkg in $extra_system_packages; do
+    echo "Install $pkg by apk"
+    apk --no-cache add "$pkg"
+  done
+fi
+
+if [ -n "$extra_packages" ]; then
+  warn "Input 'extra_packages' is deprecated. We now build LaTeX document with full TeXLive installed."
+fi
+
+if [ -n "$working_directory" ]; then
+  cd "$working_directory"
+fi
+
+# shellcheck disable=SC2086
+if [ -n "$root_file" ]; 
+  then
+    echo "Running root_file $root_file"
+    "$compiler" $args "$root_file"
+  else
+    echo "Running changed_files: $changed_files"
+    while IFS='' read -r LINE || [ -n "${LINE}" ]; do
+      echo "processing line: ${LINE}"
+      cd "$(dirname "${LINE}")";
+      "$compiler" $args "$(basename ${LINE})"; 
+      cd -
+    done <  $changed_files
+fi
+
